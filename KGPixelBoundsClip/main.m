41e5fb55f369ffa4ef5686294eef99be54873a0a
David Keegan <dkeegan@signnow.com>
2012-10-05 20:18:04 -0700

    Initial Commit

---
+++ KGPixelBoundsClip/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  KGPixelBoundsClip
+//
+//  Created by David Keegan on 10/5/12.
+//  Copyright (c) 2012 David Keegan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
