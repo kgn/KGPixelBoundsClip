295a8520bdd0719a1a78958943dafac99968ce34
David Keegan <dkeegan@signnow.com>
2012-10-05 23:13:50 -0700

    first pass at working code!

--- NSImage+PixelBoundsClip.m
+++ NSImage+PixelBoundsClip.m
@@ -11,7 +11,43 @@
 @implementation NSImage(PixelBoundsClip)
 
 - (NSImage *)imageClippedToPixelBounds{
-    return self;
+    NSBitmapImageRep *bitmapImage = [[NSBitmapImageRep alloc] initWithData:[self TIFFRepresentation]];
+    NSInteger width = [bitmapImage pixelsWide];
+    NSInteger height = [bitmapImage pixelsHigh];
+
+    NSRect clipRect = CGRectZero;
+    clipRect.origin.x = clipRect.size.width = round(width*0.5);
+    clipRect.origin.y = clipRect.size.height = round(height*0.5);
+    for(NSUInteger x = 0; x < width; ++x){
+        for(NSUInteger y = 0; y < height; ++y){
+            CGFloat red, green, blue, alpha;            
+            NSColor *color = [bitmapImage colorAtX:x y:y];
+            [color getRed:&red green:&green blue:&blue alpha:&alpha];
+            if(alpha > 0){
+                if(x < clipRect.origin.x){
+                    clipRect.origin.x = x;
+                }else if(x > clipRect.size.width){
+                    clipRect.size.width = x;
+                }
+
+                if(y < clipRect.origin.y){
+                    clipRect.origin.y = y;
+                }else if(y > clipRect.size.height){
+                    clipRect.size.height = y;
+                }
+            }
+        }
+    }
+
+    clipRect.size.width -= clipRect.origin.x;
+    clipRect.size.height -= clipRect.origin.y;
+
+    CGImageRef imageRef = [self CGImageForProposedRect:nil context:nil hints:nil];
+    CGImageRef croppedImageRef = CGImageCreateWithImageInRect(imageRef, clipRect);
+    CGImageRelease(imageRef), imageRef = nil;
+    NSImage *croppedImage = [[NSImage alloc] initWithCGImage:croppedImageRef size:NSZeroSize];
+    CGImageRelease(croppedImageRef), croppedImageRef = nil;
+    return croppedImage;
 }
 
 @end
